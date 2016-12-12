ec3be824b2b93611334dafe3154fb4656cd31718
Luke Doleman <luke@dolemandesign.com>
2016-12-12 23:53:49 +0000

    Upgraded from Jade -> Pug due to the name change + started implementing socket.io for data passing from client

--- SeeTweetApp/routes/index.js
+++ SeeTweetApp/routes/index.js
@@ -1,11 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var Twitter = require('twitter');
-var client = new Twitter({
-  consumer_key: 'izFoNkDtE3ZPo0incOx03Z0on',
-  consumer_secret: 'YsBXESCPXk93j9y7qRQQMnUv0LD3c9EpsPMcOIf44WGFfJdgDQ',
-  bearer_token: 'AAAAAAAAAAAAAAAAAAAAAFT%2FxgAAAAAAG5djI51WpTcUGgbTNw%2BP3n30Xss%3DrLomzfrUXc1Ul2uWQ52S5gBYm3W0ru5afyCR0ziQ7DfzrPffRt'
-});
 var request = require('request');
 
 router.get('/', function(req, res){
@@ -13,37 +7,5 @@ router.get('/', function(req, res){
     title: 'Home'
   });
 });
-
-router.get('/timeline', function(req, res, next) {
-  client.get('statuses/user_timeline', { screen_name: 'dmanisking', count: 320}, function(error, tweets, response) {
-    if (!error) {
-      var favs=[]; var text=[];var retweets=[];
-      console.log(tweets);
-      var thumbnail = tweets[0].user.profile_image_url;
-      var user = tweets[0].user.name;
-      var description = tweets[0].user.description;
-      var followers = tweets[0].user.followers_count;
-      for(var i = 0; i < tweets.length;i++){
-                text[i] = tweets[i].text;
-        favs[i] = tweets[i].favorite_count;
-        retweets[i] = tweets[i].retweet_count;
-      }
-      res.status(200).render('layout', {title:'Timeline',
-      tweets: text, favs: favs, retweets:retweets, test:tweets,
-      user:user, pic:thumbnail, followers:followers,
-      description:description});
-    }
-    else {
-      res.status(500).json({ error: error });
-    }
-  });
-});
-
-// PROBLEMS
-// Can't pass data from front to back to front
-// (for username info)
-//
-// Can't the one page multiple times (for many different calls)
-//
 
 module.exports = router;
---
+++ SeeTweetApp/routes/timeline.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var Twitter = require('twitter');
+var client = new Twitter({
+  consumer_key: 'izFoNkDtE3ZPo0incOx03Z0on',
+  consumer_secret: 'YsBXESCPXk93j9y7qRQQMnUv0LD3c9EpsPMcOIf44WGFfJdgDQ',
+  bearer_token: 'AAAAAAAAAAAAAAAAAAAAAFT%2FxgAAAAAAG5djI51WpTcUGgbTNw%2BP3n30Xss%3DrLomzfrUXc1Ul2uWQ52S5gBYm3W0ru5afyCR0ziQ7DfzrPffRt'
+});
+
+
+router.get('/', function(req, res, next) {
+  client.get('statuses/user_timeline', { screen_name: 'nodejs', count: 320}, function(error, tweets, response) {
+    if (!error) {
+      var favs=[]; var text=[];var retweets=[];
+      console.log(tweets);
+      var thumbnail = tweets[0].user.profile_image_url;
+      var user = tweets[0].user.name;
+      var description = tweets[0].user.description;
+      var followers = tweets[0].user.followers_count;
+      for(var i = 0; i < tweets.length;i++){
+                text[i] = tweets[i].text;
+        favs[i] = tweets[i].favorite_count;
+        retweets[i] = tweets[i].retweet_count;
+      }
+      res.status(200).render('timeline', {title:'Timeline',
+      tweets: text, favs: favs, retweets:retweets, test:tweets,
+      user:user, pic:thumbnail, followers:followers,
+      description:description});
+    }
+    else {
+      res.status(500).json({ error: error });
+    }
+  });
+});
+
+module.exports = router;
