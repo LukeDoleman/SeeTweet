9cdb3a8bf4319159d4f1f63f2fbe2c2f8f449a53
Luke Doleman <luke@dolemandesign.com>
2017-01-09 16:59:05 +0000

    Updated mention creation and now converts to json

--- SeeTweetApp/routes/timeline.js
+++ SeeTweetApp/routes/timeline.js
@@ -15,7 +15,7 @@ router.get('/', function(req, res, next) {
   var twitter_handle = req.param('username');
   client.get('statuses/user_timeline', { screen_name: twitter_handle, count: 320}, function(error, tweets, response) {
     if (!error) {
-      var favs=[]; var text=[];var retweets=[];
+      var favs=[];var text=[];var retweets=[];
       var thumbnail = tweets[0].user.profile_image_url;
       var user = tweets[0].user.name;
       var description = tweets[0].user.description;
@@ -33,57 +33,20 @@ router.get('/', function(req, res, next) {
             if (arrayMatch in mentions) {
               mentions[arrayMatch] = mentions[arrayMatch] += 1;
             } else {
-
+              //[{"@luke":1},{"@evan":2},{"@john":2}]
               //HERE can add a node to the list of dictionaries
               //Might aswell also add picture and any other stuff for the graph here in future
-
               mentions[arrayMatch] = 1;
             }
           }
         }
       }
-
-      // res.status(200);
-
-      //console.log(mentions);
-
-      /*jshint loopfunc: true */
-      // ^ Prevents function called in loop error warning, is this okay???
-      // var f=0;
-      // console.log(mentions.length);
-      // for (var mention in mentions) {
-      //   f++;
-      //   //console.log(f);
-      //   client.get('statuses/user_timeline', { screen_name: mention, count: 10}, function(error, tweets, response) {
-      //     if (!error) {
-      //       // console.log(mention);
-      //       // console.log(tweets[0].text);
-      //       for(var k = 0; k < tweets.length;k++) {
-      //         var stringMatch = (tweets[k].text).match(pattern);
-      //         if (!!stringMatch) {
-      //           for (var l = 0; l < stringMatch.length;l++) {
-      //             var arrayMatch = stringMatch[l];
-      //             if (arrayMatch in mentions) {
-      //               mentions[arrayMatch] = mentions[arrayMatch] += 1;
-      //             } else {
-      //               console.log(arrayMatch);
-      //               mentions[arrayMatch] = 1;
-      //             }
-      //           }
-      //         }
-      //       }
-      //       res.status(200);
-      //     } else {
-      //       console.log("ERROR: " + mention);
-      //       res.status(500).json({error:error});
-      //     }
-      //   });
-      // }
-
-      //console.log(mentions);
-      jsonfile.writeFile('public/info/mentions.json', mentions, function (err) {
-        console.error(err);
-      });
+      console.log(typeof mentions);
+      jmentions = JSON.stringify(mentions);
+      console.log(typeof jmentions);
+      // jsonfile.writeFile('public/info/mentions.json', mentions, function (err) {
+      //   console.error(err);
+      // });
       res.status(200).render('timeline', {title:'Timeline',
         tweets: text, favs: favs, retweets:retweets, test:tweets,
         user:user, pic:thumbnail, followers:followers,
@@ -92,7 +55,68 @@ router.get('/', function(req, res, next) {
       res.status(500).json({ error: error });
     }
   });
-
 });
 
 module.exports = router;
+
+// res.status(200);
+
+//console.log(mentions);
+
+/*jshint loopfunc: true */
+// ^ Prevents function called in loop error warning, is this okay???
+// var f=0;
+// console.log(mentions.length);
+// for (var mention in mentions) {
+//   f++;
+//   //console.log(f);
+//   client.get('statuses/user_timeline', { screen_name: mention, count: 10}, function(error, tweets, response) {
+//     if (!error) {
+//       // console.log(mention);
+//       // console.log(tweets[0].text);
+//       for(var k = 0; k < tweets.length;k++) {
+//         var stringMatch = (tweets[k].text).match(pattern);
+//         if (!!stringMatch) {
+//           for (var l = 0; l < stringMatch.length;l++) {
+//             var arrayMatch = stringMatch[l];
+//             if (arrayMatch in mentions) {
+//               mentions[arrayMatch] = mentions[arrayMatch] += 1;
+//             } else {
+//               console.log(arrayMatch);
+//               mentions[arrayMatch] = 1;
+//             }
+//           }
+//         }
+//       }
+//       res.status(200);
+//     } else {
+//       console.log("ERROR: " + mention);
+//       res.status(500).json({error:error});
+//     }
+//   });
+// }
+
+// for(var i=0;i<tweets.length;i++) {
+//   text[i] = tweets[i].text;
+//   favs[i] = tweets[i].favorite_count;
+//   retweets[i] = tweets[i].retweet_count;
+//   var stringMatch = (tweets[i].text).match(pattern);
+//   if (!!stringMatch) {
+//     for (var j=0;j<stringMatch.length;j++) {
+//       var arrayMatch = stringMatch[j];
+//       if (mentions.length !== 0){
+//         for (var mention in mentions) {
+//           if (arrayMatch === Object.keys(mentions[mention])[0]) {
+//             mentions[mention][Object.keys(mentions[mention])[0]] =
+//             mentions[mention][Object.keys(mentions[mention])[0]] + 1;
+//           } else {
+//             //[{"@luke":1},{"@evan":2},{"@john":2}]
+//             mentions.push({[arrayMatch]:1});
+//           }
+//         }
+//       } else {
+//         mentions.push({[arrayMatch]:1});
+//       }
+//     }
+//   } console.log(mentions);
+// }
