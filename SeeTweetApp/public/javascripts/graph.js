aa43c1cbc6d742c5413a130ad7f1090e19a65129
Luke Doleman <luke@dolemandesign.com>
2017-01-10 21:25:38 +0000

    Force directed graph of nodes for each mention works, handle appears on hover, JSON parse issue still present

--- SeeTweetApp/public/javascripts/graph.js
+++ SeeTweetApp/public/javascripts/graph.js
@@ -2,6 +2,11 @@ var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
 
+var simulation = d3.forceSimulation()
+    //.force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
 d3.json('info/mentions.json', function(error, people) {
 
   var jpeople = JSON.parse(people);
@@ -20,4 +25,18 @@ d3.json('info/mentions.json', function(error, people) {
       .enter().append("circle")
       .attr("r", 5)
       .attr("fill", "purple");
+
+  node.append("title")
+      .text(function(d) { return d.user; });
+
+  simulation
+      .nodes(jpeople.handles)
+      .on("tick", ticked);
+
+  function ticked() {
+      node
+          .attr("cx", function(d) { return d.x; })
+          .attr("cy", function(d) { return d.y; });
+    }
+
 });
