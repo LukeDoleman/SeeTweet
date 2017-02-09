7a686a30ee74a9641fd95c49b63ae1f6821bb373
Luke Doleman <luke@dolemandesign.com>
2017-02-09 23:46:57 +0000

    Added top 5 tweet support and Tweets in total / Tweets per day placeholder

--- SeeTweetApp/public/javascripts/stats.js
+++ SeeTweetApp/public/javascripts/stats.js
@@ -1,8 +1,15 @@
-$( window ).on( "load", function() {
-  twttr.widgets.createTweet(
-      '20',
-      document.getElementById("tweet-wrapper"), {
-          theme: 'light'
-      }
-  );
+$(window).on( "load", function() {
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+  console.log(local_metrics[0][0]);
+  //var name = local_metrics[0][0];
+  for (var i=0;i<local_metrics[0].length;i++) {
+    twttr.widgets.createTweet(
+        local_metrics[0][i],
+        document.getElementById("tweet-wrapper"), {
+            theme: 'light',
+        }
+    );
+  }
 });
--- SeeTweetApp/routes/statistics.js
+++ SeeTweetApp/routes/statistics.js
@@ -18,16 +18,20 @@ var async = require('async');
 function getMaxMetric(tweets_list) {
   var tweet_ids = [];
   for (var i=0;i<3;i++) {
+    var max_val = [];
     var internal_ids = [];
     for (var x=0;x<5;x++) {
       var max = 0;
+      var max_id;
       for(var value in tweets_list) {
         var val = tweets_list[value][i];
-        if (val > max && internal_ids.indexOf(val) == -1 ) {
+        if (val > max && max_val.indexOf(val) == -1 ) {
           max = val;
+          max_id = value;
         }
       }
-      internal_ids.push(max);
+      max_val.push(max);
+      internal_ids.push(max_id);
     }
     tweet_ids.push(internal_ids);
   }
@@ -44,12 +48,14 @@ router.get('/', function(req, res, next) {
       var description = tweets[0].user.description;
       var followers = tweets[0].user.followers_count;
       var tweet_ids_max = {};
+      console.log(tweets);
       for(var i = 0; i < tweets.length;i++) {
         if (tweets[i].text.substring(0,2) != "RT" ) {
           var popularity = tweets[i].retweet_count + tweets[i].favorite_count;
-          tweet_ids_max[tweets[i].id] = [tweets[i].retweet_count, tweets[i].favorite_count, popularity];
+          tweet_ids_max[tweets[i].id_str] = [tweets[i].retweet_count, tweets[i].favorite_count, popularity];
         }
       }
+      console.log(tweet_ids_max);
       var full_ids = getMaxMetric(tweet_ids_max);
       console.log(full_ids);
       res.status(200).render('statistics', {title:'Statistics', metrics:full_ids});
