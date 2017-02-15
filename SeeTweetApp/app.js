094ae36465d47805f2be4219a4f03145525a151f
Luke Doleman <luke@dolemandesign.com>
2017-02-15 14:45:40 +0000

    Added New UI gradients instead of light blue one. Added temporary Insert item to dictionary for Mongodb, works correctly.

--- SeeTweetApp/app.js
+++ SeeTweetApp/app.js
@@ -67,11 +67,12 @@ app.use(function(err, req, res, next) {
   });
 });
 
-var url = 'mongodb://localhost:27017/test';
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server.");
-  db.close();
-});
+//Connects to the server and the database "tweetdb"
+// var url = 'mongodb://localhost:27017/tweetdb';
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected correctly to server.");
+//   db.close();
+// });
 
 module.exports = app;
--- SeeTweetApp/routes/index.js
+++ SeeTweetApp/routes/index.js
@@ -1,8 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var MongoClient = require('mongodb').MongoClient;
+var test = require('assert');
 
 router.get('/', function(req, res){
+
+
+  MongoClient.connect('mongodb://localhost:27017/tweetdb', function(err, db) {
+    var collection = db.collection("simple_document_insert_collection_no_safe");
+    // Insert a single document
+    collection.insertOne({hello:'world_no_safe'});
+
+    // Wait for a second before finishing up, to ensure we have written the item to disk
+    setTimeout(function() {
+
+      // Fetch the document
+      collection.findOne({hello:'world_no_safe'}, function(err, item) {
+        console.log("Success! - " + item.hello);
+        test.equal(null, err);
+        test.equal('world_no_safe', item.hello);
+        db.close();
+      });
+    }, 100);
+  });
+
   res.render('index', {
     title: 'Home'
   });
